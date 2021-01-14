cce0afdb92cf62da5219940c3c7aa93396a1fa39
Louis Irwin <coding@louisirwin.co.uk>
2021-01-14 19:53:20 +0000

    🎉 Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,113 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Coronavirus Vaccine Progress in England</h1>
+    <Graph :data="data" v-if="data" style="height: 65vh; width: 90vw; margin-left:5vw;" :width="100" :height="100" />
+    <div v-else>
+      Loading
+    </div>
+    <div style="text-align: left;">
+      <div id="credit">
+        Created by <b><a href="mailto:coding@louisirwin.co.uk">Louis Irwin</a></b><br>
+        Code available open source on <a href="https://github.com/lirwin3007">GitHub</a><br>
+        First dose data is sourced from the <a href="https://coronavirus.data.gov.uk/details/developers-guide">government coronavirus data API</a>
+      </div>
+
+      <div id="warning">
+        <b>WARNING</b><br>
+        All other data is the result of very basic modelling. I am in no way a modelling expert, so all other data should be treated the same as you would a <b>wild guess</b>.<br> Please read the notes below and <b>come to your own conclusion</b> as to if the models used are representative of reality.
+      </div>
+
+      <div id="notes">
+        <div @click="showNotes=!showNotes" style="cursor: pointer;"><b>Modelling Notes {{ !showNotes ? '(Click to expand)' : '' }}</b></div>
+
+        <span v-if="showNotes">
+          <br>'Protected from severe disease' is calculated by taking 70% of the total number of people who had received a vaccine 3 weeks ago.
+          This is in line with section 4.7 of <a href="https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/951928/uk-covid-19-vaccines-delivery-plan-final.pdf">this government report</a><br><br>
+
+          Predictions are made using exponential regression. i.e. new data is predicted by the exponential curve that best fits the current data.
+          This does not take into account many real life effects such as the availability of vaccine and efficiency of distribution.<br><br>
+
+          This prediction is limited to 25 days in an effort to show this method is not reliable predicting far into the future - however even this limit of 25 days probably well over-estimates this models ability to predict anything!<br><br>
+
+          Numbers of people in each priority cohort is taken from <a href="https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/951928/uk-covid-19-vaccines-delivery-plan-final.pdf">this government report</a>
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import Graph from './components/Graph.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Graph,
+  },
+  data() {
+    return {
+      data: null,
+      showNotes: false,
+    };
+  },
+  mounted() {
+    const baseUrl = "https://api.coronavirus.data.gov.uk/v1";
+    const structure = `{"date": "date", "firstdose": "cumPeopleVaccinatedFirstDoseByPublishDate", "seconddose": "cumPeopleVaccinatedSecondDoseByPublishDate"}`;
+    const filters = "areaType=nation;areaName=england";
+    axios.get(`${baseUrl}/data?structure=${encodeURIComponent(structure)}&filters=${filters}`)
+      .then(response => {
+        this.data = [
+          {
+            date: "2020-12-08",
+            firstdose: 0,
+            seconddose: 0
+          },
+          ...response.data.data
+        ];
+      })
+      .catch(function (error) {
+        alert("Oops... Something went wrong whilst loading the data.")
+        console.log(error);
+      });
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+}
+
+a {
+  text-decoration-style: dashed;
+  text-decoration-color: #5e5e5e;
+  color: black;
+  font-weight: bold;
+}
+
+#credit {
+  margin: 1rem;
+}
+
+#warning {
+  margin: 1rem;
+  background-color: rgb(255, 136, 136);
+  border: 3px solid rgb(141, 44, 44);
+  border-radius: 0.5rem;
+  padding: 1rem;
+}
+
+#notes {
+  margin: 1rem;
+  background-color: rgb(136, 164, 255);
+  border: 3px solid rgb(44, 54, 141);
+  border-radius: 0.5rem;
+  padding: 1rem;
 }
 </style>
---
+++ src/components/Graph.vue
@@ -0,0 +1,178 @@
+<script>
+import { Line } from 'vue-chartjs';
+import regression from 'regression';
+
+export default {
+  extends: Line,
+  props: {
+    data: Array
+  },
+  data() {
+    return {
+      options: {
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'Date'
+            },
+            type: 'time',
+          }],
+          yAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'Number of people'
+            },
+            ticks: {
+              min: 0,
+              max: 70000000,
+            },
+          }],
+        },
+      }
+    };
+  },
+  mounted () {
+    this.renderChart(this.chartData, this.options);
+  },
+  methods: {
+    createStartDatapoint(time, value) {
+      return {
+        t: time,
+        y: value
+      }
+    }
+  },
+  computed: {
+    orderedData() {
+      return this.data.slice().sort((a, b) => new Date(a.date) - new Date(b.date));
+    },
+    startDate() {
+      return this.orderedData[0].date;
+    },
+    endDate() {
+      var result = new Date(this.prediction[this.prediction.length - 1].t);
+      result.setDate(result.getDate() + 3 * 7);
+      return result;
+    },
+    prediction() {
+      var model = regression.exponential(this.orderedData.map(dataPoint => [
+        ((new Date(dataPoint.date)).getTime() - (new Date("2020-12-01")).getTime()) / 100000,
+        dataPoint.firstdose == 0 ? 1 : dataPoint.firstdose / 100000
+      ]),
+      {
+        precision: 25
+      });
+
+      const lastDate = this.orderedData[this.orderedData.length - 1];
+      var counter = 0;
+      var total = lastDate.firstdose;
+      var result = [];
+
+      var currentDate = new Date(lastDate.date);
+      while (counter < 25 && total < 55980000) {
+        const time = (new Date(currentDate) - (new Date("2020-12-01")).getTime()) / 100000;
+        var prediction = model.predict(time)[1] * 100000;
+        if (prediction > 55980000) {
+          prediction = 55980000;
+        }
+        result.push({
+          t: new Date(currentDate),
+          y: prediction
+        });
+
+        currentDate.setDate(currentDate.getDate() + 1);
+        counter += 1;
+        total = prediction;
+      }
+
+      return result;
+    },
+    chartData() {
+      return {
+        datasets: [
+          {
+            label: "Population (England)",
+            backgroundColor: "#F0F0F055",
+            borderDash: [10, 10],
+            data: [
+              this.createStartDatapoint(this.startDate, 55980000),
+              this.createStartDatapoint(this.endDate, 55980000)
+            ]
+          },
+          {
+            label: "Vaccine Priority Cohorts 1-4 (88% of mortality)",
+            borderColor: "#101010AA",
+            borderDash: [5, 5],
+            fill: false,
+            data: [
+              this.createStartDatapoint(this.startDate, 12000000),
+              this.createStartDatapoint(this.endDate, 12000000)
+            ]
+          },
+          {
+            label: "Vaccine Priority Cohorts 1-9 (99% of mortality)",
+            borderColor: "#101010AA",
+            borderDash: [5, 15],
+            fill: false,
+            data: [
+              this.createStartDatapoint(this.startDate, 27000000),
+              this.createStartDatapoint(this.endDate, 27000000)
+            ]
+          },
+          {
+            label: "First Dose",
+            backgroundColor: "#FFD70055",
+            borderColor: "#FFD700FF",
+            data: this.orderedData.map(dataPoint => ({
+              t: dataPoint.date,
+              y: dataPoint.firstdose
+            })),
+          },
+          {
+            label: "Protected from severe disease",
+            backgroundColor: "#00FF0055",
+            borderColor: "#00FF00FF",
+            data: this.orderedData.map(dataPoint => {
+              var dateFor = new Date(dataPoint.date);
+              dateFor.setDate(dateFor.getDate() + 3 * 7);
+              return {
+                t: dateFor,
+                y: parseInt(dataPoint.firstdose * 0.7)
+              }
+            }),
+          },
+          {
+            label: "[PREDICTION] First Dose",
+            backgroundColor: "#80808055",
+            borderColor: "#ffe8baFF",
+            data: [
+              ...this.prediction,
+              ...(this.prediction[this.prediction.length - 1].y == 55980000 ?
+              [{
+                t: this.endDate,
+                y: this.prediction[this.prediction.length - 1].y
+              }] :
+              [])
+            ],
+          },
+          {
+            label: "[PREDICTION] Protected from severe disease",
+            backgroundColor: "#80808055",
+            borderColor: "#b5ffb5FF",
+            data: this.prediction.map(dataPoint => {
+              var dateFor = new Date(dataPoint.t);
+              dateFor.setDate(dateFor.getDate() + 3 * 7);
+              return {
+                t: dateFor,
+                y: parseInt(dataPoint.y * 0.7)
+              }
+            }),
+          },
+        ],
+      };
+    },
+  },
+}
+</script>
